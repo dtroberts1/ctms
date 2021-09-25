bd94c15a62e764a60d1bbf245c3df1d714fef58b
Dylan Roberts <dylan@robertssoftwaresolutions.com>
2021-09-25 11:29:26 -0400

    CTMS-2-Added nice doughnut charts

--- app/src/app/pie-chart/pie-chart.component.ts
+++ app/src/app/pie-chart/pie-chart.component.ts
@@ -1,7 +1,11 @@
 import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
-import { ArcElement, Chart, PieController,  } from 'chart.js';
+import { ArcElement, Chart, PieController, DoughnutController, Tooltip, Legend } from 'chart.js';
 Chart.register(PieController);
 Chart.register(ArcElement);
+Chart.register(DoughnutController);
+Chart.register(Tooltip);
+Chart.register(Legend);
+
 
 @Component({
   selector: 'app-pie-chart',
@@ -9,30 +13,111 @@ Chart.register(ArcElement);
   styleUrls: ['./pie-chart.component.less']
 })
 export class PieChartComponent implements AfterViewInit {
-  @ViewChild('pieCanvas') private pieCanvas!: ElementRef;
-  pieChart: any;
+  @ViewChild('popularityChartCanvas') 
+  private popularityChartCanvas!: ElementRef;
+  @ViewChild('tasteTestChartCanvas') 
+  private tasteTestChartCanvas!: ElementRef;
+
+  popularityChart: any;
+  tasteTestChart: any;
+
 
   constructor() { }
 
   ngAfterViewInit(): void {
     this.pieChartBrowser();
   }
+  getColors(length: number, pallet: string[]){
+    let colors = [];
+
+    for(let i = 0; i < length; i++) {
+      colors.push(pallet[i % pallet.length]);
+    }
+
+    return colors;
+  }
+
   pieChartBrowser(): void {
-    this.pieChart = new Chart(this.pieCanvas.nativeElement, {
-      type: 'pie',
+    let pal = ["#001464", "#26377B", "#404F8B", "#59709A", "#8CA0B9", "#B2C7D0", "#CCDDE0"]
+
+    this.popularityChart = new Chart(this.popularityChartCanvas.nativeElement, {
+      type: 'doughnut',
       data: {
-        labels: ['Apple', 'Google', 'Facebook', 'Infosys', 'Hp', 'Accenture'],
+        labels: ['Apple', 'Google', 'Facebook', 'Infosys'],
         datasets: [{
-          backgroundColor: [
-            '#2ecc71',
-            '#3498db',
-            '#95a5a6',
-            '#9b59b6',
-            '#f1c40f',
-            '#e74c3c'
-          ],
-          data: [12, 19, 3, 17, 28, 24]
+          backgroundColor: this.getColors(4, pal),
+          data: [12, 19, 3, 17],
+          label: 'Popularity',
+          hoverBorderWidth: 5,
+          hoverBorderColor: '#42b7ff',
+          hoverOffset: 15,
         }]
+      },
+      options: {
+        maintainAspectRatio: true,
+        layout: {
+          padding: {
+            left: 30,
+            right: 30,
+            top: 30,
+            bottom: 30,
+          }
+        },
+        plugins: {
+          legend:{
+            display: true,
+            position: 'right',
+            title: {
+              text: 'Popularity',
+              display: true,
+              font: {
+                size: 22,
+              }
+            },
+            
+          }
+          
+        }
+      }
+    });
+
+    this.tasteTestChart = new Chart(this.tasteTestChartCanvas.nativeElement, {
+      type: 'doughnut',
+      
+      data: {
+        labels: ['Apple', 'Google', 'Facebook', 'Infosys'],
+        datasets: [{
+          backgroundColor: this.getColors(4, pal),
+          data: [12, 19, 3, 17],
+          label: 'Rankings',
+          hoverBorderWidth: 5,
+          hoverBorderColor: '#42b7ff',
+          hoverOffset: 15,
+        }]
+      },
+      options: {
+        maintainAspectRatio: true,
+        layout: {
+          padding: {
+            left: 30,
+            right: 30,
+            top: 30,
+            bottom: 30,
+          }
+        },
+        plugins: {
+          legend:{
+            display: true,
+            position: 'right',
+            title: {
+              text: 'Popularity',
+              display: true,
+              font: {
+                size: 22,
+              }
+            },
+          },         
+        }
       }
     });
   }
